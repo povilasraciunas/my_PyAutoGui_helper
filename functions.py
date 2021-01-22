910026701ad06ab8671b30a57a39545b1a312bd8
Povilas <povilasraciunas@gmail.com>
2021-01-22 16:30:42 +0200

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,14 @@
+import pyautogui
+
+#Functions with examples for using with pyAutoGui
+
+start_button = ('start_button.png')
+
+# find_img_n_click - finds image from file on screen, clicks center
+def find_img_n_click(image):
+    location = pyautogui.locateOnScreen(image)
+    print(location)
+    location_x, location_y = pyautogui.center(location)
+    pyautogui.click(x=location_x, y=location_y)
+
+find_img_n_click(start_button) 

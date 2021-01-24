ba60232dc4ff1d614db25321bf569710427c2603
Povilas <povilasraciunas@gmail.com>
2021-01-24 15:13:19 +0200

    Added screen_stamp function

--- functions.py
+++ functions.py
@@ -1,20 +1,23 @@
 ### Functions with examples for using with PyAutoGui ###
-import pyautogui
+import pyautogui # pyautogui - main module
+import time # time - module for getting system time
 
-# find_img_n_click - finds image from file on screen, clicks center of the image.
-def find_img_n_click(image):
+## find_n_click ## - finds image from file on screen, clicks center of the image.
+def find_n_click(image):
     location = pyautogui.locateOnScreen(image)
     print(location)
     location_x, location_y = pyautogui.center(location)
     pyautogui.click(x=location_x, y=location_y)
+
 # Example image path:
 start_button = ('start_button.png')
 # Example function call:
-find_img_n_click(start_button) 
+find_n_click(start_button) 
 
-# Screenshot with time stamp
-#def scree_stamp():
-import time
-timestr = time.strftime("%Y%m%d-%H%M%S")
-print timestr
+## screen_stamp ## - makes a screenshot of main monitor and saves it in .png format with system date&time in the filename.
+def screen_stamp():
+    system_time = time.strftime("%Y%m%d-%H%M%S")
+    screen_with_timestamp = pyautogui.screenshot(system_time+'.png')
+# Example function call:    
+screen_stamp()
 

fe9a5f02ca92586e3eff264712dd29fe1159df95
Povilas <povilasraciunas@gmail.com>
2021-01-24 15:55:08 +0200

    Added open_chrome function

--- functions.py
+++ functions.py
@@ -1,8 +1,9 @@
 ### Functions with examples for using with PyAutoGui ###
 import pyautogui # pyautogui - main module
 import time # time - module for getting system time
+import os # os - module for passing command to windows command line(CMD)
 
-## find_n_click ## - finds image from file on screen, clicks center of the image.
+## find_n_click(image) ## - finds image from file on screen, clicks center of the image.
 def find_n_click(image):
     location = pyautogui.locateOnScreen(image)
     print(location)
@@ -14,10 +15,20 @@ start_button = ('start_button.png')
 # Example function call:
 find_n_click(start_button) 
 
-## screen_stamp ## - makes a screenshot of main monitor and saves it in .png format with system date&time in the filename.
+## screen_stamp() ## - makes a screenshot of main monitor and saves it in .png format with system date&time in the filename.
 def screen_stamp():
     system_time = time.strftime("%Y%m%d-%H%M%S")
-    screen_with_timestamp = pyautogui.screenshot(system_time+'.png')
+    pyautogui.screenshot(system_time+'.png')
 # Example function call:    
 screen_stamp()
+
+## open_chrome(url) ## - opens new with window of chrome with provided URL
+def open_chrome(url):
+  
+    os.system(f'"start chrome --new-window --incognito {url}"')
+
+# Example url
+url = "http://www.google.com"
+# Example function call:    
+open_chrome(url)
 

c9109cfe1d3c3c4a27b6805c9808ca4536dcf9ff
Povilas <povilasraciunas@gmail.com>
2021-01-24 14:33:58 +0200

    Added venv to gitignore

--- functions.py
+++ functions.py
@@ -1,8 +1,5 @@
+### Functions with examples for using with PyAutoGui ###
 import pyautogui
-
-#Functions with examples for using with pyAutoGui
-
-start_button = ('start_button.png')
 
 # find_img_n_click - finds image from file on screen, clicks center of the image.
 def find_img_n_click(image):
@@ -10,5 +7,14 @@ def find_img_n_click(image):
     print(location)
     location_x, location_y = pyautogui.center(location)
     pyautogui.click(x=location_x, y=location_y)
-
+# Example image path:
+start_button = ('start_button.png')
+# Example function call:
 find_img_n_click(start_button) 
+
+# Screenshot with time stamp
+#def scree_stamp():
+import time
+timestr = time.strftime("%Y%m%d-%H%M%S")
+print timestr
+
